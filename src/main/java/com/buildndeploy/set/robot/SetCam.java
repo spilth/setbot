87812023f60e0e25ca13575e50f7cda146849510
Brian Kelly <polymonic@gmail.com>
2011-08-26 08:52:46 -0400

    Codes map to Set cards

--- src/main/java/com/buildndeploy/set/robot/SetCam.java
+++ src/main/java/com/buildndeploy/set/robot/SetCam.java
@@ -1,22 +1,33 @@
 package com.buildndeploy.set.robot;
 
 import java.util.HashMap;
+import java.util.List;
+
+import com.buildndeploy.set.Card;
+import com.buildndeploy.set.Deck;
 
 import topcodes.TopCode;
 
 public class SetCam {
 	static TopCode[] validCodes;
-	static HashMap<Integer, Integer> cardCodeMap;
+	static HashMap<Integer, Integer> codeToCardNumberMap;
+	static List<Card> cards;
 	
 	static {
+		cards = new Deck().getCards();
+		
 		validCodes = TopCode.generateCodes();
-		cardCodeMap = new HashMap<Integer, Integer>(99);
+		codeToCardNumberMap = new HashMap<Integer, Integer>(99);
 		for (int i = 0; i < validCodes.length; i++) {
-			cardCodeMap.put(validCodes[i].getCode(), i + 1);
+			codeToCardNumberMap.put(validCodes[i].getCode(), i);
 		}	
 	}
 	
 	public static int getCodeCardNumber(int code) {		
-		return cardCodeMap.get(code);
+		return codeToCardNumberMap.get(code);
+	}
+
+	public static Object getCodeCard(int code) {
+		return cards.get(getCodeCardNumber(code));
 	}
 }
--- src/test/java/com/buildndeploy/set/robot/SetCamTest.java
+++ src/test/java/com/buildndeploy/set/robot/SetCamTest.java
@@ -4,12 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.buildndeploy.set.Deck;
+
 public class SetCamTest {
 
 	@Test
 	public void codeMapsToCardNumber() {
-		assertEquals(1, SetCam.getCodeCardNumber(31));
-		assertEquals(2, SetCam.getCodeCardNumber(47));
+		assertEquals(0, SetCam.getCodeCardNumber(31));
+		assertEquals(1, SetCam.getCodeCardNumber(47));
+	}
+
+	@Test
+	public void codeMapsToCard() {
+		assertEquals(new Deck().getCards().get(0), SetCam.getCodeCard(31));
 	}
 
 }
